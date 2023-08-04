6bf4d4af6a77cdf971b7a1331f65328066d07fe7
naziranwer <122814116+naziranwer@users.noreply.github.com>
2023-08-04 23:09:33 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  fs.appenFile(fileName,",An online learning platform.",(err)=>{
+    if(err){
+      console.log(err);
+    }
+  })
   
 };
 
